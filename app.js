aca8234f8b7b1f8752631a52ed3e628205ce19c6
Howard0127 <howard740127@gmail.ocm>
2021-10-25 21:00:55 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const Record = require('./models/record')
+const Category = require('./models/Category')
 
 const app = express()
 
@@ -20,11 +23,33 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
     .catch(err => console.error(err))
+})
+
+app.get('/records/new', (req, res) => {
+  Category.find()
+    .lean()
+    .then(categories => res.render('new', { categories }))
+    .catch(err => console.error(err))
+})
+
+app.post('/records', (req, res) => {
+  console.log(req.body)
+  const { name, date, category, amount } = req.body
+  Record.create({
+    name,
+    date,
+    category,
+    amount
+  }) 
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 app.listen(3000, () => {
