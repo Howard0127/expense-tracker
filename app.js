46a9056027e2ac940f5bf712dd78e7966042f683
Howard0127 <howard740127@gmail.ocm>
2021-10-27 10:21:45 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Record.find()
     .lean()
+    .sort({ _id: 'desc'})
     .then(records => res.render('index', { records }))
     .catch(err => console.error(err))
 })
