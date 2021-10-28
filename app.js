064c1e0eecff9f13935dba62651cb8e8520190b5
Howard0127 <howard740127@gmail.ocm>
2021-10-28 13:20:20 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const routes = require('./routes')
 
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
@@ -18,7 +19,7 @@ app.use(methodOverride('_method'))
 
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
 
